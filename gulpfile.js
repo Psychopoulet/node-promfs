08bf8f0448b0dfdfb919d0b45e1d5adacb8303df
Psychopoulet <sikymanager@gmail.com>
2017-07-31 19:43:53 +0200

    add travis && delete native instructions

--- gulpfile.js
+++ gulpfile.js
@@ -3,75 +3,82 @@
 
 // deps
 
-	const	path = require("path"),
+	const path = require("path");
 
-			gulp = require("gulp"),
-			plumber = require("gulp-plumber"),
+	// gulp
+	const gulp = require("gulp");
+	const plumber = require("gulp-plumber");
 
-			babel = require("gulp-babel"),
-			
-			eslint = require("gulp-eslint"),
-			mocha = require("gulp-mocha");
+	// tests
+	const istanbul = require("gulp-istanbul");
+	const eslint = require("gulp-eslint");
+	const mocha = require("gulp-mocha");
 
-// private
+	// compilation
+	const babel = require("gulp-babel");
+	
+// consts
 
-	var _gulpFile = path.join(__dirname, "gulpfile.js"),
-		_libDir = path.join(__dirname, "lib"),
-			_libFiles = path.join(_libDir, "*.js"),
-		_distDir = path.join(__dirname, "dist"),
-			_distFiles = path.join(_distDir, "*.js"),
-		_unitTestsFiles = path.join(__dirname, "tests", "*.js"),
-		_toTestFiles = [_gulpFile, _libFiles, _unitTestsFiles];
+	const UNITTESTSFILES = path.join(__dirname, "tests", "*.js");
+
+	const TOTESTFILES = [
+		path.join(__dirname, "gulpfile.js"),
+		path.join(__dirname, "lib", "*.js"),
+		UNITTESTSFILES
+	];
 
 // tasks
 
-	gulp.task("babel", () => {
+	gulp.task("eslint", () => {
 
-		return gulp.src(_libFiles)
-			.pipe(babel({
-				presets: ["es2015"]
-			}))
-			.pipe(gulp.dest(_distDir));
-
-	});
-
-	gulp.task("eslint", ["babel"], () => {
-
-		return gulp.src(_toTestFiles)
+		return gulp.src(TOTESTFILES)
 			.pipe(plumber())
 			.pipe(eslint({
+				"env": require(path.join(__dirname, "gulpfile", "eslint", "env.json")),
+				"globals": require(path.join(__dirname, "gulpfile", "eslint", "globals.json")),
 				"parserOptions": {
 					"ecmaVersion": 6
 				},
-				"rules": {
-					"linebreak-style": 0,
-					"quotes": [ 1, "double" ],
-					"indent": 0,
-					// "indent": [ 2, "tab" ],
-					"semi": [ 2, "always" ]
-				},
-				"env": {
-					"node": true, "es6": true, "mocha": true
-				},
-				"extends": "eslint:recommended"
+				// http://eslint.org/docs/rules/
+				"rules": require(path.join(__dirname, "gulpfile", "eslint", "rules.json"))
+
 			}))
 			.pipe(eslint.format())
 			.pipe(eslint.failAfterError());
 
 	});
 
-	gulp.task("mocha", ["eslint"], () => {
+	gulp.task("istanbul", [ "eslint" ], () => {
 
-		return gulp.src(_unitTestsFiles)
+		return gulp.src(TOTESTFILES)
+			.pipe(istanbul())
+			.pipe(istanbul.hookRequire());
+
+	});
+
+	gulp.task("mocha", [ "istanbul" ], () => {
+
+		return gulp.src(UNITTESTSFILES)
 			.pipe(plumber())
-			.pipe(mocha({reporter: "spec"}));
+			.pipe(mocha())
+			.pipe(istanbul.writeReports());
+
+	});
+
+	gulp.task("babel", [ "mocha" ], () => {
+
+		return gulp.src(path.join(__dirname, "lib", "*.js"))
+			.pipe(babel({
+				presets: ["es2015"]
+			}))
+			.pipe(gulp.dest(path.join(__dirname, "dist")));
 
 	});
 
 // watcher
 
 	gulp.task("watch", () => {
-		gulp.watch(_toTestFiles, ["mocha"]);
+		gulp.watch(TOTESTFILES, ["eslint"]);
 	});
 
 
